7ec03a800fc79e5839d70deb08fc5ae0eddb4673
Idham Mansyah Awwalu <59715715+idhammansyah@users.noreply.github.com>
2021-03-26 20:23:06 +0700

    Add files via upload

---
+++ js/kuis.js
@@ -0,0 +1,169 @@
+ (function () {
+     var questions = [{
+         question: "Apa yang singkatan dari Severe acute respiratory syndrome coronavirus 2 ?",
+         choices: ["SARS-CoV-2", "MERS", "CoV-2", "SARS-2", "ABRI-0"],
+         correctAnswer: 0
+     }, {
+         question: "Dari mana virus (SARS-CoV-2) datang ?",
+         choices: ["Australia", "Malaysia", "Hongkong", "China", "Korea Utara"],
+         correctAnswer: 3
+     }, {
+         question: "Pengertian dari infeksi virus corona adalah",
+         choices: ["Copet20", "Flu", "Covid-19", "Batuk", "Pusing"],
+         correctAnswer: 2
+     }, {
+         question: "Apa gejala Covid-19 ?",
+         choices: ["Demam diatas 38 derajat", "Muntah", "Pegal-pegal", "Kesemutan", "Mules"],
+         correctAnswer: 0
+     }, {
+         question: "Bila berhendakk keluar kota, alangkah baiknya kita tes apa ?",
+         choices: ["Kehamilan", "Muntaber", "Swab Antigen", "Minum Antimo", "Tidur yang cukup"],
+         correctAnswer: 2
+     }];
+
+     var questionCounter = 0; //Tracks question number
+     var selections = []; //Array containing user choices
+     var quiz = $('#quiz'); //Quiz div object
+
+     // Display initial question
+     displayNext();
+
+     // Click handler for the 'next' button
+     $('#next').on('click', function (e) {
+         e.preventDefault();
+
+         // Suspend click listener during fade animation
+         if (quiz.is(':animated')) {
+             return false;
+         }
+         choose();
+
+         // If no user selection, progress is stopped
+         if (isNaN(selections[questionCounter])) {
+             alert('Please make a selection!');
+         } else {
+             questionCounter++;
+             displayNext();
+         }
+     });
+
+     // Click handler for the 'prev' button
+     $('#prev').on('click', function (e) {
+         e.preventDefault();
+
+         if (quiz.is(':animated')) {
+             return false;
+         }
+         choose();
+         questionCounter--;
+         displayNext();
+     });
+
+     // Click handler for the 'Start Over' button
+     $('#start').on('click', function (e) {
+         e.preventDefault();
+
+         if (quiz.is(':animated')) {
+             return false;
+         }
+         questionCounter = 0;
+         selections = [];
+         displayNext();
+         $('#start').hide();
+     });
+
+     // Animates buttons on hover
+     $('.button').on('mouseenter', function () {
+         $(this).addClass('active');
+     });
+     $('.button').on('mouseleave', function () {
+         $(this).removeClass('active');
+     });
+
+     // Creates and returns the div that contains the questions and
+     // the answer selections
+     function createQuestionElement(index) {
+         var qElement = $('<div>', {
+             id: 'question'
+         });
+
+         var header = $('<h4>Pertanyaan ' + (index + 1) + ':</h4>');
+         qElement.append(header);
+
+         var question = $('<h5>').append(questions[index].question);
+         qElement.append(question);
+
+         var radioButtons = createRadios(index);
+         qElement.append(radioButtons);
+
+         return qElement;
+     }
+
+     // Creates a list of the answer choices as radio inputs
+     function createRadios(index) {
+         var radioList = $('<ul>');
+         var item;
+         var input = '';
+         for (var i = 0; i < questions[index].choices.length; i++) {
+             item = $('<li>');
+             input = '<input type="radio" name="answer" value=' + i + ' />';
+             input += questions[index].choices[i];
+             item.append(input);
+             radioList.append(item);
+         }
+         return radioList;
+     }
+
+     // Reads the user selection and pushes the value to an array
+     function choose() {
+         selections[questionCounter] = +$('input[name="answer"]:checked').val();
+     }
+
+     // Displays next requested element
+     function displayNext() {
+         quiz.fadeOut(function () {
+             $('#question').remove();
+
+             if (questionCounter < questions.length) {
+                 var nextQuestion = createQuestionElement(questionCounter);
+                 quiz.append(nextQuestion).fadeIn();
+                 if (!(isNaN(selections[questionCounter]))) {
+                     $('input[value=' + selections[questionCounter] + ']').prop('checked', true);
+                 }
+
+                 // Controls display of 'prev' button
+                 if (questionCounter === 1) {
+                     $('#prev').show();
+                 } else if (questionCounter === 0) {
+
+                     $('#prev').hide();
+                     $('#next').show();
+                 }
+             } else {
+                 var scoreElem = displayScore();
+                 quiz.append(scoreElem).fadeIn();
+                 $('#next').hide();
+                 $('#prev').hide();
+                 $('#start').show();
+             }
+         });
+     }
+
+     // Computes score and returns a paragraph element to be displayed
+     function displayScore() {
+         var score = $('<p>', {
+             id: 'question'
+         });
+
+         var numCorrect = 0;
+         for (var i = 0; i < selections.length; i++) {
+             if (selections[i] === questions[i].correctAnswer) {
+                 numCorrect++;
+             }
+         }
+
+         score.append('Kamu Menjawab ' + numCorrect + ' pertanyaan dari ' +
+             questions.length + ' yang benar! Selamat!');
+         return score;
+     }
+ })();
